9ba1b5aa07971555a59bf71a4b412fbb325d4999
Iqbal Faisal <faisaelz@gmail.com>
2022-08-18 08:36:43 +0700

    day6

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(request, response){
+    response.send("Hello World")
+})
+
+app.listen(8000, function()
+{
+    console.log("server running");
+})
