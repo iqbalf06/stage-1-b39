9c95b4777f5eac96a09ae68706abb4862cf095e9
Iqbal Faisal <faisaelz@gmail.com>
2022-08-22 17:43:23 +0700

    day8

--- index.js
+++ index.js
@@ -9,53 +9,131 @@ app.use(express.urlencoded({extended: false}))
 
 
 let isLogin = true
+
 let dataProject = []
 
 app.get('/', function(request, response){
-    response.render('index')
+console.log(dataProject);
+
+    let data =  dataProject.map(function(item){
+        return {
+            ...item,
+            isLogin,
+            duration: getDistanceTime(new Date(item.Startdate),new Date(item.Enddate))
+        }
+
+   })
+
+console.log(data);
+    response.render('index', {isLogin, dataProject: data})
 })
 
 app.get('/contact', function(request, response){
     response.render('contact')
 })
 
-
 app.get('/add-project', function(request, response){
-    response.render('add-project', {isLogin})
-})
-app.get('/myproject-detail', function(request, response){
-    response.render('myproject-detail', {isLogin})
+    
+    response.render('add-project', {isLogin, dataProject} )
 })
 
-app.get('/myproject-detail/:id', function(request, response){
-    let id = request.params.id
-    console.log(id);
+app.post('/add-project', function(request, response) {
 
-    response.render('myproject-detail', {
-        id,
-        title: 'Selamat Datang',
-        startdate: '23-05-2022',
-        enddate: '01-09-2022',
-        content: 'lorem ipsum',
-        technologies : 'javascript, node js',
-    })
-
-})
-
-app.get('/add-project', function(request, response){
-    response.render('add-project')
-})
-
-app.post('/add-project', function(request, response){
-    console.log(request.body);
     let title = request.body.inputTitle
+    let Startdate = request.body.Startdate
+    let Enddate = request.body.Enddate
     let content = request.body.inputContent
 
-    console.log(title);
-    console.log(content);
 
-    response.redirect('/add-project')
+    let project = {
+        title,
+        Startdate,
+        Enddate,
+        content,
+    }
+    
+    dataProject.push(project)
+    response.redirect('/')
 })
+
+
+app.get('/myproject-detail/:index', function(request, response){
+    let index = request.params.index
+
+    let data = dataProject[index] 
+    data.duration = getDistanceTime(new Date(data.Startdate),new Date(data.Enddate)) 
+    data.startdate = new Date (data.startdate)
+    data.enddate = new Date(data.enddate)
+
+    response.render('myproject-detail',{data})
+})
+
+app.get('/update-project/:index', function(request, response){
+    let index = request.params.index
+
+    let data = {
+        title: dataProject[index].title,
+        startdate: dataProject[index].startdate,
+        enddate: dataProject[index].enddate,
+        content: dataProject[index].content
+
+    }
+
+    response.render('update-project', {index, data})
+})
+
+app.post('/update-project/:index', function(request, response){
+
+    let index = request.params.index
+
+    dataProject[index].title = request.body.inputTitle
+    dataProject[index].startdate = request.body.Startdate
+    dataProject[index].enddate = request.body.Enddate
+    dataProject[index].content = request.body.inputContent
+
+
+    response.redirect('/')
+})
+
+app.get('/delete-project/:index', function(request, response){
+    let index = request.params.index
+    dataProject.splice(index, 1)
+
+    response.redirect('/')
+})
+
+
+
+function getDistanceTime(Startdate, Enddate){
+
+    let stDate = Startdate
+    let enDate = Enddate
+  
+    let distance = enDate - stDate
+    console.log(distance);
+  
+    let milisecond = 1000 // 1 detik 1000 milisecond
+    let secondInHours = 3600 // 1 jam sama dengan 3600 detik
+    let hoursInDay = 24 // 1 hari 24 jam
+    let dayInMonth = 30
+    let monthInYear = 12
+  
+    let distanceYear = Math.floor(distance / (milisecond * secondInHours * hoursInDay * dayInMonth * monthInYear))
+    let distanceMonth = Math.floor(distance / (milisecond * secondInHours * hoursInDay * dayInMonth))
+    let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay))
+  
+  
+    if(distanceYear > 0){
+  
+      return `${distanceYear} year`
+    } else if(distanceMonth > 0){
+        return `${distanceMonth} month`
+    } else {
+        return `${distanceDay} day`
+    }
+     
+  }
+
 
 app.listen(port, function(){
     console.log(`server running on port ${port}`);
