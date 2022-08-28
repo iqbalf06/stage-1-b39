1c7778384e6654d98de3ed933ea33bea14e134da
Iqbal Faisal <faisaelz@gmail.com>
2022-08-29 05:53:48 +0700

    day11

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const db = require('./connection/db')
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+const flash = require('express-flash')
 
 const app = express()
 const port = 8000
@@ -7,33 +11,44 @@ app.set('view engine', 'hbs') // set view engine hbs
 app.use('/assets', express.static(__dirname + '/assets')) // path folder assets
 app.use(express.urlencoded({extended: false}))
 
-const db = require('./connection/db')
+app.use(flash())
 
-let isLogin = true
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 2 * 60 * 60 * 1000 // 2JAM
+    }
+}))
 
+
+db.connect(function (err, client, done) {
+    if (err) throw err // menampilkan error koneksi database
 
 app.get('/', function(request, response){
 
-
+    
     db.connect(function(err, client, done){
         if (err) throw err // menampilkan error koneksi database
 
+        console.log(request.session);
         client.query('SELECT * FROM tb_projects ORDER BY id DESC', function(err, result){
             if (err) throw err // menampilkan error dari query
-            console.log(result.rows);
+            // console.log(result.rows);
             let data = result.rows
 
             let dataProject = data.map(function(item){
                 return {
                     ...item,
                     duration: getDistanceTime(item.start_date, item.end_date),
-                    isLogin,
+                    isLogin: request.session.isLogin,
                     
                 }
 
             })
             
-            response.render('index', {isLogin, dataProject})
+            response.render('index', {dataProject, user: request.session.user, isLogin: request.session.isLogin})
  
         }) 
       
@@ -42,11 +57,15 @@ app.get('/', function(request, response){
 })
 
 app.get('/contact', function(request, response){
-    response.render('contact')
+    response.render('contact', {user: request.session.user, isLogin: request.session.isLogin})
 })
 
-app.get('/add-project', function(request, response){     
-   response.render('add-project')
+app.get('/add-project', function(request, response){ 
+    if(!request.session.user) {
+        request.flash('danger', 'Anda belum login! Silahkan login terlebih dahulu')
+        return response.render('login')
+    }    
+   response.render('add-project', {user: request.session.user, isLogin: request.session.isLogin})
 })
 
 app.post('/add-project', function(request, response) {
@@ -84,14 +103,16 @@ app.get('/myproject-detail/:id', function(request, response){
             let dataProject = data.map(function (item){
                 return {
                     ...item,
+                    start_date: getFullTime(item.start_date),
+                    end_date: getFullTime(item.end_date),
                     duration: getDistanceTime(item.start_date, item.end_date),
-                    isLogin,
+                    isLogin: request.session.isLogin,
                     
                 }
 
             })
                 
-              response.render('myproject-detail', {data: dataProject[0]})
+              response.render('myproject-detail', {data: dataProject[0], user: request.session.user, isLogin: request.session.isLogin})
         }) 
       
     })
@@ -99,6 +120,11 @@ app.get('/myproject-detail/:id', function(request, response){
 })
 
 app.get('/update-project/:idParams', function(request, response){
+    if(!request.session.user){
+        request.flash('danger', 'Silahkan login!')
+        return response.redirect('/login')
+    }
+    
     let id = request.params.idParams
 
     db.connect(function(err, client, done){
@@ -109,11 +135,11 @@ app.get('/update-project/:idParams', function(request, response){
         client.query(query, function(err, result){
             if (err) throw err // menampilkan error dari query
 
-            let data = result.rows
+            let data = result.rows[0]
             
             
                 
-            response.render('update-project', {data: data[0]})
+            response.render('update-project', { data,user: request.session.user, isLogin: request.session.isLogin })
         }) 
       
     })
@@ -124,18 +150,20 @@ app.get('/update-project/:idParams', function(request, response){
 app.post('/update-project/:idParams', function(request, response){
     let id = request.params.idParams
 
+    let {inputTitle, inputContent } = request.body
+
     db.connect(function(err, client, done){
         if (err) throw err // menampilkan error koneksi database
 
-        let query = `UPDATE * FROM tb_projects WHERE id=${id}`
+        let query = `UPDATE tb_projects
+        SET title='${inputTitle}', content='${inputContent}'
+        WHERE id=${id}`
 
         client.query(query, function(err, result){
             if (err) throw err // menampilkan error dari query
-
-            let data = result.rows
         
                 
-            response.redirect('/', {data: data[0]})
+            response.redirect('/')
         }) 
       
     })
@@ -161,7 +189,87 @@ app.get('/delete-project/:idParams', function(request, response){
 
 })
 
+app.get('/register', function (request, response) {
+    response.render('register')
+})
 
+app.post('/register', function (request, response) {
+    
+    db.connect(function(err, client, done){
+        if (err) throw err // menampilkan error koneksi database
+
+        let { inputName, inputEmail, inputPassword } = request.body
+        
+        const hashedPassword = bcrypt.hashSync(inputPassword, 10)
+
+        let query = `INSERT INTO tb_user (name, email, password)
+        VALUES ('${inputName}','${inputEmail}','${hashedPassword}')`
+
+        client.query(query, function(err, result){
+            if (err) throw err // menampilkan error dari query
+                
+            response.redirect('register')
+        }) 
+      
+    })
+    
+    
+})
+
+app.get('/login', function (request, response) {
+    response.render('login')
+})
+
+app.post('/login', function (request, response) {
+
+    let { inputEmail, inputPassword } = request.body
+   
+    let query = `SELECT * FROM tb_user WHERE email='${inputEmail}'`
+
+    client.query(query, function(err, result){
+        if (err) throw err // menampilkan error dari query
+
+        console.log((result.rows[0]));
+        if(result.rows.length == 0) {
+             console.log('Email belum terdaftar')
+             request.flash('danger', 'Email belum terdaftar')
+             return response.redirect('login')
+             
+        }
+
+        const isMatch = bcrypt.compareSync(inputPassword, result.rows[0].password)
+        console.log(isMatch)
+
+        if(isMatch) {
+            console.log('Login berhasil');
+
+            request.session.isLogin = true
+            request.session.user = {
+                id:  result.rows[0].id,
+                name: result.rows[0].name,
+                email: result.rows[0].email,
+            }
+
+            request.flash('success', 'Login berhasil')
+            response.redirect('/')
+
+        } else {
+            console.log('Password salah');
+            request.flash('danger', 'Password salah')
+            response.redirect('login')
+        }
+            
+       
+    }) 
+
+})
+
+app.get('/logout', function (request, response) {
+    request.session.destroy()
+    response.redirect('/')
+})
+
+})
 
 function getDistanceTime(startdate, enddate){
 
@@ -192,6 +300,29 @@ function getDistanceTime(startdate, enddate){
     }
      
   }
+
+  function getFullTime(time) {
+
+    let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"]
+
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    if (hours < 10) {
+        hours = "0" + hours
+    } else if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+
+    // 12 Agustus 2022 09.04
+    let fullTime = `${date} ${month[monthIndex]} ${year}`
+    // console.log(fullTime);
+    return fullTime
+}
 
 
 app.listen(port, function(){
